f640d0ff8c6f3f20378c861f8e181dea75b4d9e3
Armando Salinas <armandosalinas84@gmail.com>
2023-07-09 22:35:23 -0500

    seventh commit

--- frontend/src/components/Square.jsx
+++
@@ -1,9 +0,0 @@
-function Square({ val, chooseSquare, winSquare }) {
-  return (
-    <div className={winSquare ? "squareWin" : "square"} onClick={chooseSquare}>
-      {val}
-    </div>
-  );
-}
-
-export default Square;
