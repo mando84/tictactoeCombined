7c9deda7fb0c69eed6a6604a71732f7acf2453d8
Armando Salinas <47874700+mando84@users.noreply.github.com>
2023-08-03 16:38:44 -0500

    Update Login.jsx changed login page message

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -61,7 +61,7 @@ function Login() {
         <h1>
           <FaSignInAlt /> Login
         </h1>
-        <p>Login hey and start setting goals</p>
+        <p>Login and start playing</p>
       </section>
 
       <section className="form">
