01db5c46767feb1a32883d35b4529874d85db0b9
Armando Salinas <47874700+mando84@users.noreply.github.com>
2023-08-03 17:03:09 -0500

    Update Login.jsx "changed message on Login"

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -61,7 +61,7 @@ function Login() {
         <h1>
           <FaSignInAlt /> Login
         </h1>
-        <p>Login and start playing</p>
+        <p>Login and start playing TicTacToe</p>
       </section>
 
       <section className="form">
